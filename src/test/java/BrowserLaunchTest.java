7a6211e8fb8af7524f10b3bd06b6cc700fde934e
Vinayreddy129 <vinayreddy6126@gmail.com>
2024-11-08 15:08:15 +0530

    inial commit

---
+++ src/test/java/BrowserLaunchTest.java
@@ -0,0 +1,41 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class BrowserLaunchTest {
+
+    WebDriver driver; // Declare WebDriver instance
+
+    @BeforeMethod
+    public void setUp() {
+        // Set up the ChromeDriver path (adjust for your operating system)
+        System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+
+        // Initialize the WebDriver
+        driver = new ChromeDriver();
+
+        // Maximize browser window
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    public void openHrm() throws InterruptedException {
+        // Open a website
+        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+        Thread.sleep(5000);
+
+        // Optionally, you could verify the title of the page
+        String pageTitle = driver.getTitle();
+        System.out.println("Page title is: " + pageTitle);
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        // Close the browser
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
